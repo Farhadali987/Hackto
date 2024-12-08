e08be39882ce525cdb7ae46dba94d974d5491dc4
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2024-12-09 01:02:11 +0500

    header footer resposive ness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,46 +10,26 @@ import {
 const Header = () => {
   return (
     <div className="font-sans">
-      {/* upper Header */}
-      <div className="flex items-center justify-center lg:justify-around bg-[#7E33E0] text-white">
-        <div className="lg:flex items-center gap-6 hidden">
+      {/* Upper Header */}
+      <div className="flex flex-wrap items-center justify-center lg:justify-between bg-[#7E33E0] text-white px-4">
+        <div className="hidden lg:flex items-center gap-6">
           <div className="flex items-center gap-2">
-            <Image
-              src={"/icons/envelope.svg"}
-              alt="env"
-              width={16}
-              height={16}
-            />
+            <Image src={"/icons/envelope.svg"} alt="env" width={16} height={16} />
             <p>akbardal@gmail.com</p>
           </div>
-          <div className="flex items-center gap-4">
-            <Image
-              src={"/icons/phone.svg"}
-              alt="phone"
-              width={16}
-              height={16}
-            />
+          <div className="flex items-center gap-2">
+            <Image src={"/icons/phone.svg"} alt="phone" width={16} height={16} />
             <p>(0321)3011912</p>
           </div>
         </div>
         <div className="flex items-center gap-4 py-3">
           <div className="flex items-center gap-1">
             <p>English</p>
-            <Image
-              src={"/icons/arrow-down.svg"}
-              alt="env"
-              width={16}
-              height={16}
-            />
+            <Image src={"/icons/arrow-down.svg"} alt="env" width={16} height={16} />
           </div>
           <div className="flex items-center gap-1">
             <p>USD</p>
-            <Image
-              src={"/icons/arrow-down.svg"}
-              alt="env"
-              width={16}
-              height={16}
-            />
+            <Image src={"/icons/arrow-down.svg"} alt="env" width={16} height={16} />
           </div>
           <div className="flex items-center gap-1">
             <p>Login</p>
@@ -66,212 +46,162 @@ const Header = () => {
           </div>
         </div>
       </div>
-      {/* Header  */}
-      <div className=" flex flex-col items-center py-5">
-        <div className="flex items-center gap-4 sm:gap-10 md:gap-20">
-          <div className="flex items-center gap-1">
+
+      {/* Main Header */}
+      <div className="flex flex-col items-center py-5 px-4 lg:px-10">
+        <div className="flex items-center justify-between w-full lg:w-auto gap-4 sm:gap-10">
+          {/* Logo */}
+          <div className="flex items-center">
             <Image src={"/images/logo.png"} alt="Logo" width={98} height={34} />
           </div>
-          <div className="flex">
-            <ul className="hidden lg:flex items-center space-x-8 text-[#0D0E43] font-semibold">
-              <li>
-                <Link
-                  href={"/"}
-                  className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                >
-                  <p>Home</p>
-                  <Image
-                    src={"/icons/arrow-down-p.svg"}
-                    alt="env"
-                    width={16}
-                    height={16}
-                    className="invisible group-hover:visible"
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"/pages/shopgrid"}
-                  className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                >
-                  <p>Pages</p>
-                  <Image
-                    src={"/icons/arrow-down-p.svg"}
-                    alt="env"
-                    width={16}
-                    height={16}
-                    className="invisible group-hover:visible"
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={""}
-                  className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                >
-                  <p>Products</p>
-                  <Image
-                    src={"/icons/arrow-down-p.svg"}
-                    alt="env"
-                    width={16}
-                    height={16}
-                    className="invisible group-hover:visible"
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"/blog"}
-                  className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                >
-                  <p>Blog</p>
-                  <Image
-                    src={"/icons/arrow-down-p.svg"}
-                    alt="env"
-                    width={16}
-                    height={16}
-                    className="invisible group-hover:visible"
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"/pages/shoplist"}
-                  className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                >
-                  <p>Shop</p>
-                  <Image
-                    src={"/icons/arrow-down-p.svg"}
-                    alt="env"
-                    width={16}
-                    height={16}
-                    className="invisible group-hover:visible"
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"contact"}
-                  className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                >
-                  <p>Contact</p>
-                  <Image
-                    src={"/icons/arrow-down-p.svg"}
-                    alt="env"
-                    width={16}
-                    height={16}
-                    className="invisible group-hover:visible"
-                  />
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="flex items-center border-2 corder-[#E7E6EF] ">
-            <input type="text" className="outline-none" />
+
+          {/* Navigation */}
+          <ul className="hidden lg:flex items-center space-x-8 text-[#0D0E43] font-semibold">
+            <li>
+              <Link
+                href={"/"}
+                className="group flex items-center gap-1 hover:text-[#FB2E86]"
+              >
+                <p>Home</p>
+                <Image
+                  src={"/icons/arrow-down-p.svg"}
+                  alt="env"
+                  width={16}
+                  height={16}
+                  className="invisible group-hover:visible"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/pages/shopgrid"}
+                className="group flex items-center gap-1 hover:text-[#FB2E86]"
+              >
+                <p>Pages</p>
+                <Image
+                  src={"/icons/arrow-down-p.svg"}
+                  alt="env"
+                  width={16}
+                  height={16}
+                  className="invisible group-hover:visible"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={""}
+                className="group flex items-center gap-1 hover:text-[#FB2E86]"
+              >
+                <p>Products</p>
+                <Image
+                  src={"/icons/arrow-down-p.svg"}
+                  alt="env"
+                  width={16}
+                  height={16}
+                  className="invisible group-hover:visible"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/blog"}
+                className="group flex items-center gap-1 hover:text-[#FB2E86]"
+              >
+                <p>Blog</p>
+                <Image
+                  src={"/icons/arrow-down-p.svg"}
+                  alt="env"
+                  width={16}
+                  height={16}
+                  className="invisible group-hover:visible"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/pages/shoplist"}
+                className="group flex items-center gap-1 hover:text-[#FB2E86]"
+              >
+                <p>Shop</p>
+                <Image
+                  src={"/icons/arrow-down-p.svg"}
+                  alt="env"
+                  width={16}
+                  height={16}
+                  className="invisible group-hover:visible"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/contact"}
+                className="group flex items-center gap-1 hover:text-[#FB2E86]"
+              >
+                <p>Contact</p>
+                <Image
+                  src={"/icons/arrow-down-p.svg"}
+                  alt="env"
+                  width={16}
+                  height={16}
+                  className="invisible group-hover:visible"
+                />
+              </Link>
+            </li>
+          </ul>
+
+          {/* Search Bar */}
+          <div className="hidden lg:flex items-center border-2 border-[#E7E6EF] rounded">
+            <input
+              type="text"
+              placeholder="Search"
+              className="outline-none px-2 py-1"
+            />
             <div className="bg-[#FB2E86] p-1">
               <Image
                 src={"/icons/search.svg"}
-                alt="Logo"
+                alt="Search"
                 width={24}
                 height={24}
               />
             </div>
           </div>
+
+          {/* Hamburger Menu */}
           <Sheet>
             <SheetTrigger className="lg:hidden">
-              <Image src={"/icons/ham.svg"} alt="env" width={16} height={16} />
+              <Image src={"/icons/ham.svg"} alt="menu" width={24} height={24} />
             </SheetTrigger>
             <SheetContent>
               <SheetDescription>
-                <ul className="flex flex-col items-center justify-around h-[60vh] text-[#0D0E43] font-semibold">
+                <ul className="flex flex-col items-center gap-4 text-[#0D0E43] font-semibold">
                   <li>
-                    <Link
-                      href={"/"}
-                      className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                    >
-                      <p>Home</p>
-                      <Image
-                        src={"/icons/arrow-down-p.svg"}
-                        alt="env"
-                        width={16}
-                        height={16}
-                        className="invisible group-hover:visible"
-                      />
+                    <Link href={"/"} className="hover:text-[#FB2E86]">
+                      Home
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      href={"/pages/shopgrid"}
-                      className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                    >
-                      <p>Pages</p>
-                      <Image
-                        src={"/icons/arrow-down-p.svg"}
-                        alt="env"
-                        width={16}
-                        height={16}
-                        className="invisible group-hover:visible"
-                      />
+                    <Link href={"/pages/shopgrid"} className="hover:text-[#FB2E86]">
+                      Pages
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      href={""}
-                      className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                    >
-                      <p>Products</p>
-                      <Image
-                        src={"/icons/arrow-down-p.svg"}
-                        alt="env"
-                        width={16}
-                        height={16}
-                        className="invisible group-hover:visible"
-                      />
+                    <Link href={"/products"} className="hover:text-[#FB2E86]">
+                      Products
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      href={"/blog"}
-                      className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                    >
-                      <p>Blog</p>
-                      <Image
-                        src={"/icons/arrow-down-p.svg"}
-                        alt="env"
-                        width={16}
-                        height={16}
-                        className="invisible group-hover:visible"
-                      />
+                    <Link href={"/blog"} className="hover:text-[#FB2E86]">
+                      Blog
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      href={"/pages/shoplist"}
-                      className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                    >
-                      <p>Shop</p>
-                      <Image
-                        src={"/icons/arrow-down-p.svg"}
-                        alt="env"
-                        width={16}
-                        height={16}
-                        className="invisible group-hover:visible"
-                      />
+                    <Link href={"/pages/shoplist"} className="hover:text-[#FB2E86]">
+                      Shop
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      href={"/contact"}
-                      className="group flex items-center gap-1 hover:text-[#FB2E86]"
-                    >
-                      <p>Contact</p>
-                      <Image
-                        src={"/icons/arrow-down-p.svg"}
-                        alt="env"
-                        width={16}
-                        height={16}
-                        className="invisible group-hover:visible"
-                      />
+                    <Link href={"/contact"} className="hover:text-[#FB2E86]">
+                      Contact
                     </Link>
                   </li>
                 </ul>
